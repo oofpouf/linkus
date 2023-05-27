6ecaa2bafc832e380033622cfaa669f25af3acc9
Chong Tze Yi <cherie@Chongs-MacBook-Pro.local>
2023-05-28 00:29:04 +0800

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:linkus/view/login_view.dart';
 import 'package:linkus/view/register_view.dart';
-import 'package:linkus/view/verify_email_view.dart';
-import 'dart:developer' as devtools
-    show log; // so that you can call devtools.log instead of just log
+import 'package:linkus/view/verify_email_view.dart'; // so that you can call devtools.log instead of just log
 
 import 'firebase_options.dart';
 
@@ -22,6 +20,7 @@ void main() {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/main/': (context) => const MainView(),
       },
     ),
   );
