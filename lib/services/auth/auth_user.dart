71cfbf0fabbc74ec435d8d3db9cfe3f54e0fac22
Cherie <cherrypighippo532@gmail.com>
2023-06-26 00:09:39 +0800

    unit-test-login

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -2,16 +2,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 // need to add reload function in auth user
 class AuthUser {
-  final String? email;
+  final String id;
+  final String email;
   final bool isEmailVerified;
 
   const AuthUser({
     required this.isEmailVerified,
     required this.email,
+    required this.id,
   });
 
-  factory AuthUser.fromFirebase(User user) =>
-      AuthUser(email: user.email, isEmailVerified: user.emailVerified);
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+      email: user.email!, isEmailVerified: user.emailVerified, id: user.uid);
 
   // adding the reload method
   Future<void> reload() async {
